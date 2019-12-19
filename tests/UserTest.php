625f5df02880ecf47b2961a4c502c6d85523ddaa
ROMEN <romenlee@gmail.com>
2019-12-20 01:53:01 +0200

    5.21. Dependency injection: inject objects that a class depends on

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -20,4 +20,15 @@ class UserTest extends TestCase
 
 		$this->assertEquals('', $user->getFullName());
 	}
+
+	public function testNotificationIsSent()
+	{
+		$user = new User;
+		$mock_mailer = $this->createMock(Mailer::class);
+		$mock_mailer->method('sendMessage')
+			->willReturn(true);
+		$user->setMailer($mock_mailer);
+		$user->email = 'dave@example.com';
+		$this->assertTrue($user->notify("Hello"));
+	}
 }
